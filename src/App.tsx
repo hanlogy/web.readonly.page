95aac85b9fc3985bcf815ecbb892ad0beecbbdef
Zhiguang Chen <zhiguang@hanlogy.com>
2026-02-10 12:12:24 +0100

    change folder naming to next.js style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route } from '@/lib/router';
-import { HomePage } from '@/pages/HomePage';
-import { ReaderPage } from '@/pages/ReaderPage';
-import { ResourceEditorPage } from './pages/ResourceEditorPage';
+import { HomePage } from '@/pages/home';
+import { ReaderPage } from '@/pages/read';
+import { ResourceEditorPage } from './pages/resource-editor';
 
 function App() {
   return (
